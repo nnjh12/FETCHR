5cb3162f63ff85ac7163bad2da856a9fdd561652
Bonesaw <dalantinow@gmail.com>
2019-05-17 14:03:49 -0400

    seeds, schema, model

--- models/example.js
+++ models/example.js
@@ -1,7 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var User = sequelize.define("User", {
+    name: DataTypes.STRING,
+    zipcode: DataTypes.INTEGER(6),
+    question1: DataTypes.INTEGER,
+    question2: DataTypes.INTEGER,
+    question3: DataTypes.INTEGER,
+    question4: DataTypes.INTEGER,
+    question5: DataTypes.INTEGER,
+    question6: DataTypes.INTEGER,
+    question7: DataTypes.INTEGER,
+    question8: DataTypes.INTEGER,
+    question9: DataTypes.INTEGER,
+    question10: DataTypes.INTEGER,
+    question11: DataTypes.INTEGER,
+    question12: DataTypes.INTEGER,
+    question13: DataTypes.INTEGER
   });
-  return Example;
+  return User;
 };
