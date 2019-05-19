29dd32ac64fc66ee6ab482a64c56d5b580dc8db6
Kasie <kchaplick92@gmail.com>
2019-05-19 16:38:37 -0400

    added handlebars but can't get to display at localhost

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,30 +3,48 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var survey = require("../models/survey");
-var survey = require("../models/survey");
-var survey = require("../models/survey");
-var survey = require("../models/survey");
-var survey = require("../models/survey");
+var questions = require("../models/questions");
+var choices = require("../models/choices");
+var user = require("../models/user");
+var surveyAnswers = require("../models/surveyAnswers");
+var breed = require("../models/breed");
+var breedMatch = require("../models/breedMatch");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+
+  router.get("/", function(req, res) {
+    var allQuestions;
+    var allChoices;
+    questions.all(function(data) {
+      allQuestions = data;
+    });
+    choices.all(function(data) {
+      allChoices = data;
+    });
+    var hbsObject = {
+      allQuestions: allQuestions,
+      allChoices: allChoices
+    };
+    res.render("index", hbsObject);
   });
 
-  // Load breed results page
-  app.get("/breedresults", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/breedresults.html"));
-  });
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
 
-  // Load adopt results page
-  app.get("/adoptresults", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/adoptresults.html"));
-  });
+//   // Load breed results page
+//   app.get("/breedresults", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/breedresults.html"));
+//   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+//   // Load adopt results page
+//   app.get("/adoptresults", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/adoptresults.html"));
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
 };
