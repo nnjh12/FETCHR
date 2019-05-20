5a24e6aac8e1ee36e18230ded5cedc495867bf25
Kasie <kchaplick92@gmail.com>
2019-05-20 11:47:09 -0400

    updated html route to hand off questions and their respective choices to handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,31 +4,40 @@ module.exports = function(app) {
   // Load index page
 
   app.get("/", function(req, res) {
-    // db.Question.findAll({}).then(function(dbQuestions) {
-    //   res.render("index", {
-    //     question: dbQuestions
-    //   });
-    // db.question.findAll({}).then(function(dbQuestions) {
-    //   console.log(dbQuestions);
-    //   res.render("index", {
-    //     questions: dbQuestions
-    //   });
-    // db.choice.findAll({}).then(function(dbChoices) {
-    //   console.log(dbQuestions);
-    //   res.render("index", {
-    //     choices: dbChoices
-    //   });  
-    // });
-    db.question.hasMany(db.choice, { as: "choices", foreignKey: "id" });
-    db.choice.belongsTo(db.question, { foreignKey: "id" });
+//     db.Question.findAll({}).then(function(dbQuestions) {
+//       res.render("index", {
+//         question: dbQuestions
+//       });
     db.choice
-      .findAll({ include: [db.question] })
-      .then(function(dbQuestionAndChoice) {
-        console.log(dbQuestionAndChoice);
+      .findAll({
+        // include: [db.question]
+      })
+      .then(function(dbQuestions) {
+        console.log(dbQuestions);
         res.render("index", {
-          questionAndChoice: dbQuestionAndChoice
+          questions: dbQuestions
         });
       });
+//     db.choice.findAll({}).then(function(dbChoices) {
+//       console.log(dbQuestions);
+//       res.render("index", {
+//         choices: dbChoices
+//       });  
+//     });
+    
+//     db.choice
+//       .findAll({ include: [db.question] })
+//       .then(function(dbQuestionAndChoice) {
+//         console.log(dbQuestionAndChoice);
+//         res.render("index", {
+//           questionAndChoice: dbQuestionAndChoice
+//         });
+//       });
+//   });
+// });
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
   });
 
   // Load breed results page
