cfda7deb2ae92157b44bea7fa300ab1f4021c648
Jinhwa Roh <nnjh12@gmail.com>
2019-05-22 21:14:13 -0400

    before git pull

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,14 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     db.question
       .findAll({
         include: [db.choice]
       })
-      .then(function(dbQuestions) {
-
+      .then(function (dbQuestions) {
         // console.log(dbQuestions);
         // console.log("____________________________________________________")
         // console.log(dbQuestions[0].choices);
@@ -21,17 +20,29 @@ module.exports = function(app) {
   });
 
   // Load breed results page
-  app.get("/breedresults", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/breedresults.html"));
+  app.get("/breedresults", function (req, res) {
+    db.question
+      .findAll({
+        include: [db.choice]
+      })
+      .then(function (dbQuestions) {
+        // console.log(dbQuestions);
+        // console.log("____________________________________________________")
+        // console.log(dbQuestions[0].choices);
+
+        res.render("index", {
+          questionsAndChoices: dbQuestions
+        });
+      });
   });
 
   // Load adopt results page
-  app.get("/adoptresults", function(req, res) {
+  app.get("/adoptresults", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/adoptresults.html"));
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
