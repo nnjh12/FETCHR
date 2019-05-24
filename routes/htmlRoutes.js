d07cde2bcc7d000c84df4095a13e152f465c3498
aevers3 <aevers3@users.noreply.github.com>
2019-05-24 11:42:49 -0400

    adopt.handlebars logic

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -38,15 +38,19 @@ module.exports = function (app) {
   });
 
   // Load adopt results page
-  app.get("/adoptresults", function (req, res) {
+  app.get("/adoptresults", function (req, response) {
     // petfinderRequest parameters are hardcoded for now, but will take in survey data.
     // var dogs = petfinder.petfinderRequest();
     // console.log(dogs);
     // console.log(petfinder.petfinderRequest());
+    var hbsDogs = {
+      dogs: []
+    }
+
     petfinder.petfinderRequest(19125, 5, "beagle").then(function (res) {
 
       class Dog {
-        constructor(name, age, photo, gender, status, website, phone) {
+        constructor(name, age, photo, gender, status, website, phone, address, city, state) {
           this.name = name;
           this.age = age;
           this.photo = photo;
@@ -54,6 +58,9 @@ module.exports = function (app) {
           this.status = status;
           this.website = website;
           this.phone = phone;
+          this.address = address;
+          this.city = city;
+          this.state = state;
         }
       }
 
@@ -63,24 +70,22 @@ module.exports = function (app) {
         responseDogs.push(res.animals[i]);
       }
 
-      console.log(responseDogs);
+      // console.log(responseDogs);
 
 
-      var hbsDogs = {
-        dogs: []
-      }
+      
 
-      console.log(hbsDogs.dogs);
+      // console.log(hbsDogs.dogs);
       
       for (var i = 0; i < 10; i++) {
-        hbsDogs.dogs.push(new Dog(res.animals[i].name, res.animals[i].age, res.animals[i].photos[0].small, res.animals[i].gender, res.animals[i].status, res.animals[i].url, res.animals[i].contact.phone))
+        hbsDogs.dogs.push(new Dog(res.animals[i].name, res.animals[i].age, res.animals[i].photos[0].medium, res.animals[i].gender, res.animals[i].status, res.animals[i].url, res.animals[i].contact.phone, res.animals[i].contact.address.address1, res.animals[i].contact.address.city, res.animals[i].contact.address.state))
       }
       
-      console.log(hbsDogs.dogs);
-
+      console.log(hbsDogs.dogs, "Dogs");
+      response.render("adopt", hbsDogs);
       
     })
-    res.render("adopt");
+    // console.log(hbsDogs, "after")
 
 
   });
