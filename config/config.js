3810d3931643d4d201ba220ce3b700bc40ea16ce
Kasie <kchaplick92@gmail.com>
2019-05-30 15:30:15 -0400

    env file

---
+++ config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: {
+      timestamps: false
+    }
+  }
+  // test: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   dialect: "mysql",
+  //   logging: false
+  // },
+  // production: {
+  //   // eslint-disable-next-line camelcase
+  //   use_env_variable: JAWSDB_URL,
+  //   dialect: "mysql",
+  //   define: {
+  //     timestamps: false
+  //   }
+  // }
+};
